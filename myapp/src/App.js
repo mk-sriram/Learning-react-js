9e6a6f312a623d080f90fcd5572de83fb565b006
Sriram MK <mksriram@umich.edu>
2024-01-26 01:41:18 -0500

    added files

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function wMyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+  
+
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+
+const listItems = products.map(product =>
+  <li
+    key={product.id}
+    style={{
+      color: product.isFruit ? 'magenta' : 'darkgreen'
+    }}
+  >
+    {product.title}
+  </li>
+);
+
+
+
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
+
+
+//states in reat are used to manage the current situatuion of a component 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+      <ul>{listItems}</ul>
     </div>
   );
 }
